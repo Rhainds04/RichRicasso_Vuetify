685a0bd5d0e9cd44353b146b3f1cbbe588e69cc7
Emile Rhainds <emilerhainds.work@gmail.com>
2024-11-20 19:17:34 -0500

    changes

---
+++ RichRicasso/src/components/RichRicassoCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-card class="pa-5" style="width: 100%;">
+              <div class="d-flex">
+                <div>
+                  <img src="/Images/RichRicasso.png" style="width: 300px;" alt="RichRicasso">
+                </div>
+                <div class="pa-5">
+                  <h1>Qui est Rich Ricasso</h1>
+                  <p class="font-size-1 text-justify">
+                    Rich Ricasso, le célèbre cravatier et chemisier, souhaite lancer sa boutique en ligne pour présenter sa nouvelle gamme d'été en soie unisexe.
+                    Son style, inspiré du mouvement vaporwave et vaporfashion accentue les tons pastel tels que le bleu ciel, le rose poudré et le violet lavande, et a ainsi conquis le monde de l'ultra-luxe.
+                    Il souhaite maintenant ouvrir sa gamme au grand public. Votre mission est de concevoir et réaliser cette plateforme, mettant en avant la singularité et le luxe des produits.
+                  </p>
+                </div>
+              </div>
+            </v-card>
+</template>
