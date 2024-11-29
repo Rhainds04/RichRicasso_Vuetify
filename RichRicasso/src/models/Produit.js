1c4e6818dacfc8857065336ea0433804d0fdb9c7
Chullos10 <karlosayalachozo@gmail.com>
2024-11-29 09:28:00 -0500

    Changement de la structure du produit

--- RichRicasso/src/models/Produit.js
+++ RichRicasso/src/models/Produit.js
@@ -5,7 +5,6 @@ export default class Produit {
     this.description = data.description;
     this.price = data.price;
     this.image = data.image;
-    this.size = data.size;
     this.color = data.color;
   }
 }
